a336c8fae0c835b1a32e2d3db791785f9bf14271
Abhishek Sharma <abhisheksharma41324@gmail.com>
2026-06-09 10:02:05 +0530

    [abhishek] add [first 5]

---
+++ gcr-codebase/programming-elements/level1/avg.java
@@ -0,0 +1,10 @@
+
+class avg{
+	public static void main(String[] abc){
+		int math = 94;
+		int physics = 95;
+		int chemistry = 96;
+		int avg = (math + physics + chemistry)/3;
+		System.out.println("The average is "+avg);
+	}
+}
