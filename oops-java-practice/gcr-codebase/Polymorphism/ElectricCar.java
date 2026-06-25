5ab24519d043b3a82314ad4c7c836a57140e4cce
abhi-00069 <abhisheksharma41324@gmail.com>
2026-06-25 12:31:58 +0530

    [abhi] added [ElectricCar.java]
    Refactored ElectricCar class to extend Vehicle and added fuelCost method.

--- oops-java-practice/gcr-codebase/Polymorphism/ElectricCar.java
+++ oops-java-practice/gcr-codebase/Polymorphism/ElectricCar.java
@@ -1,38 +1,10 @@
-public class Main{
+class ElectricCar extends Vehicle{
 
-    public static void main(String[] args){
+    ElectricCar(String hehe){
+        super(hehe);
+    }
 
-        Vehicle arr[]=new Vehicle[4];
-
-        arr[0]=new Car("Swift");
-        arr[1]=new Bus("Volvo");
-        arr[2]=new Bike("Pulsar");
-        arr[3]=new ElectricCar("Nexon EV");
-
-        int km=100;
-
-        for(int i=0;i<arr.length;i++){
-
-            System.out.println(arr[i].naam+" cost = "+arr[i].fuelCost(km));
-
-            if(arr[i] instanceof Car){
-                Car c=(Car)arr[i];
-                System.out.println("its a car");
-            }
-            else if(arr[i] instanceof Bus){
-                Bus b=(Bus)arr[i];
-                System.out.println("big bus");
-            }
-            else if(arr[i] instanceof Bike){
-                Bike lol=(Bike)arr[i];
-                System.out.println("bike only");
-            }
-            else if(arr[i] instanceof ElectricCar){
-                ElectricCar e=(ElectricCar)arr[i];
-                System.out.println("electric one");
-            }
-
-            System.out.println();
-        }
+    double fuelCost(int km){
+        return km*1.2;
     }
 }
