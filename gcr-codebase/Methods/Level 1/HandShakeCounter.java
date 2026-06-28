f79ab92b20b1fcf1f0f52e2b9904e12c9d4079c3
abhi-00069 <abhisheksharma41324@gmail.com>
2026-06-29 04:49:35 +0530

    [abhi] added [HandShakeCounter]

---
+++ gcr-codebase/Methods/Level 1/HandShakeCounter.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class HandShakeCounter {
+
+    static int handShake(int n)
+    {
+        return (n*(n-1))/2;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+
+        System.out.print("Students : ");
+        int n=sc.nextInt();
+
+        System.out.println("Maximum Handshakes = "+handShake(n));
+
+    }
+
+}
