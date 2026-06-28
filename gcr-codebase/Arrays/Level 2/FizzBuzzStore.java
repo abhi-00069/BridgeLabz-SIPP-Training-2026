b5ef7ec043b9889d8ed62994ec328876a16cec5e
abhi-00069 <abhisheksharma41324@gmail.com>
2026-06-29 04:15:03 +0530

    [abhi] added [FizzBuzzStore]

---
+++ gcr-codebase/Arrays/Level 2/FizzBuzzStore.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class FizzBuzzStore {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number : ");
+        int n = sc.nextInt();
+
+        if(n<=0)
+        {
+            System.out.println("Enter a positive number");
+            return;
+        }
+
+        String arr[] = new String[n+1];
+
+        for(int i=1;i<=n;i++)
+        {
+
+            if(i%3==0 && i%5==0)
+            {
+                arr[i]="FizzBuzz";
+            }
+            else if(i%3==0)
+            {
+                arr[i]="Fizz";
+            }
+            else if(i%5==0)
+            {
+                arr[i]="Buzz";
+            }
+            else
+            {
+                arr[i]=String.valueOf(i);
+            }
+
+        }
+
+        System.out.println();
+
+        for(int i=1;i<=n;i++)
+        {
+            System.out.println("Position "+i+" = "+arr[i]);
+        }
+
+    }
+
+}
