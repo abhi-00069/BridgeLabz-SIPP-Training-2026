fd2ed327554546ef5143bb9fba02c6a582bda367
abhi-00069 <abhisheksharma41324@gmail.com>
2026-06-29 04:50:38 +0530

    [

---
+++ gcr-codebase/Methods/Level 1/NumberSign.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class NumberSign {
+
+    static int check(int n)
+    {
+        if(n>0)
+            return 1;
+
+        if(n<0)
+            return -1;
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+
+        System.out.print("Enter number : ");
+        int n=sc.nextInt();
+
+        int x=check(n);
+
+        if(x==1)
+            System.out.println("Positive");
+        else if(x==-1)
+            System.out.println("Negative");
+        else
+            System.out.println("Zero");
+
+    }
+
+}
