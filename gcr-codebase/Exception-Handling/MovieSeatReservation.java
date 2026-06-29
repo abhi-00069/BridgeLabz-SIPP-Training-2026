c731d1f46b0df98d49959398e5e2ff98687b3654
abhi-00069 <abhisheksharma41324@gmail.com>
2026-06-29 05:52:07 +0530

    [abhi] added [MovieSeatReservation]

---
+++ gcr-codebase/Exception-Handling/MovieSeatReservation.java
@@ -0,0 +1,30 @@
+public class MovieSeatReservation {
+
+    static int seats[]={101,102,103,104,105};
+
+    static int getSeat(int index)
+    {
+
+        try{
+
+            return seats[index];
+
+        }
+        catch(ArrayIndexOutOfBoundsException e){
+
+            System.out.println("Invalid Seat Index");
+            return -1;
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        int seat=getSeat(8);
+
+        System.out.println("Seat = "+seat);
+
+    }
+
+}
