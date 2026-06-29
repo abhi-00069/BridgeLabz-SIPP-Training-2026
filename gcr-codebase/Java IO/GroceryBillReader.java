8d04b99230f269a4e4aad80e5a9d3b94a6a21a56
abhi-00069 <abhisheksharma41324@gmail.com>
2026-06-29 05:57:34 +0530

    [abhi] added [GroceryBillReader]

---
+++ gcr-codebase/Java IO/GroceryBillReader.java
@@ -0,0 +1,36 @@
+import java.io.*;
+
+public class GroceryBillReader {
+
+    public static void main(String[] args) {
+
+        int lines=0;
+
+        try{
+
+            BufferedReader br=new BufferedReader(new FileReader("bill.txt"));
+
+            String s;
+
+            while((s=br.readLine())!=null)
+            {
+
+                System.out.println(s);
+                lines++;
+
+            }
+
+            br.close();
+
+            System.out.println("Total Lines = "+lines);
+
+        }
+        catch(IOException e){
+
+            System.out.println("File Error.");
+
+        }
+
+    }
+
+}
