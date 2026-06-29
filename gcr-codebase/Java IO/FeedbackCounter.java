1bd47b33959e9945f63ef7a23acdd2f061a731dd
abhi-00069 <abhisheksharma41324@gmail.com>
2026-06-29 05:56:34 +0530

    [abhi] added [FeedbackCounter]

---
+++ gcr-codebase/Java IO/FeedbackCounter.java
@@ -0,0 +1,38 @@
+import java.io.*;
+
+public class FeedbackCounter {
+
+    public static void main(String[] args) {
+
+        int count=0;
+
+        try{
+
+            BufferedReader br=new BufferedReader(new FileReader("feedback.txt"));
+
+            String line;
+
+            while((line=br.readLine())!=null)
+            {
+
+                if(line.toLowerCase().contains("good"))
+                {
+                    count++;
+                }
+
+            }
+
+            br.close();
+
+            System.out.println("Good Feedback Count = "+count);
+
+        }
+        catch(IOException e){
+
+            System.out.println("File Error.");
+
+        }
+
+    }
+
+}
